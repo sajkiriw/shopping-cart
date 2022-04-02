1eec075a7eeb3a45113e84a98e68b3763470d28e
sajkiriw <kiriwendalasajith@gmail.com>
2022-04-02 12:53:20 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,27 @@
+const data = {
+  products: [
+    {
+      id: "1",
+      name: "Macbook",
+      price: 1400,
+      image:
+        "https://www.pccentre.com.na/ishoppe/image/cache/catalog/macbook-pro-350x350.jpeg",
+    },
+    {
+      id: "2",
+      name: "Car",
+      price: 2600,
+      image:
+        "https://www.amwltd.com/wp-content/uploads/2016/09/kwid-min-350x350.png",
+    },
+    {
+      id: "3",
+      name: "Shoes",
+      price: 1000,
+      image:
+        "https://i.pinimg.com/474x/16/00/ec/1600ec7c99d8448b4a552c77e480b476.jpg",
+    },
+  ],
+};
+
+export default data;
